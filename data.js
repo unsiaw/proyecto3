b6557a9bc192deb10f5e352e720b0233448806fb
matiaschap <matias.marzu@gmail.com>
2017-06-20 23:51:22 -0300

    ongs cargadas

--- data.js
+++ data.js
@@ -3,54 +3,120 @@ module.exports = [
         'model': 'Ong',
         'documents': [
             {
-                'nombre': 'ONG 1',
-                'tipo': 'Otros',
-                'telefono': '111111111',
-                'responsable': 'Juan Perez',
-                'ubicacion': 'Calle falsa 123',
-                "latitud" : "-38.7193099",
-                "longitud" : "-62.264880800000014",
-                "comentarios": []
+                "nombre": "ASOCIACION CIVIL AROE",
+                "ubicacion": "Moreno 233, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.720327",
+                "longitud": "-62.270951999999966",
+                "telefono": "4501253",
+                "responsable": "Aure Hector Ricardo",
+                "comentarios": [],
+                "tipo": "Educación"
             },
             {
-                'nombre': 'ONG 2',
-                'tipo': 'Otros',
-                'telefono': '111111111',
-                'responsable': 'Juan Perez',
-                'ubicacion': 'Calle falsa 123',
-                "latitud" : "-38.7193099",
-                "longitud" : "-62.264880800000014",
-                "comentarios": []
+                "nombre": "PROGRAMA FORESTAL DEL SUD",
+                "ubicacion": "San Martín 488, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.7210556",
+                "longitud": "-62.259360500000014",
+                "telefono": "4552993",
+                "comentarios": [],
+                "tipo": "Medio ambiente"
             },
             {
-                'nombre': 'ONG 3',
-                'tipo': 'Otros',
-                'telefono': '111111111',
-                'responsable': 'Juan Perez',
-                'ubicacion': 'Calle falsa 123',
-                "latitud" : "-38.7193099",
-                "longitud" : "-62.264880800000014",
-                "comentarios": []
+                "nombre": "ATARDECER DE LA VIDA",
+                "ubicacion": "Lautaro 3435, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.7794351",
+                "longitud": "-62.26616969999998",
+                "comentarios": [],
+                "tipo": "Ancianos"
             },
             {
-                'nombre': 'ONG 4',
-                'tipo': 'Otros',
-                'telefono': '111111111',
-                'responsable': 'Juan Perez',
-                'ubicacion': 'Calle falsa 123',
-                "latitud" : "-38.7193099",
-                "longitud" : "-62.264880800000014",
-                "comentarios": []
+                "nombre": "ACCION BAHIENSE COMUNITARIA (ABC)",
+                "ubicacion": "Donado 151, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.72181800000001",
+                "longitud": "-62.26661039999999",
+                "telefono": "156466652",
+                "responsable": "Jara Isaias",
+                "comentarios": [],
+                "tipo": "Otros"
             },
             {
-                'nombre': 'ONG 5',
-                'tipo': 'Otros',
-                'telefono': '111111111',
-                'responsable': 'Juan Perez',
-                'ubicacion': 'Calle falsa 123',
-                "latitud" : "-38.7193099",
-                "longitud" : "-62.264880800000014",
-                "comentarios": []
+                "nombre": "AGRUPACIÓN DE FLORICULTURA Y JARDINERIA",
+                "ubicacion": "Saavedra 851, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.72805",
+                "longitud": "-62.259787800000026",
+                "responsable": "Moras Olga",
+                "telefono": "154426393",
+                "comentarios": [],
+                "tipo": "Medio ambiente"
+            },
+            {
+                "nombre": "CENTRO CULTURAL SOÑAR-CREAR-HACER",
+                "ubicacion": "Vieytes 2700, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.69816790000001",
+                "longitud": "-62.29857099999998",
+                "telefono": "154144866",
+                "responsable": "Pisauri Graciela",
+                "comentarios": [],
+                "tipo": "Educación"
+            },
+            {
+                "nombre": "CENTRO COMUNITARIO SAN ROQUE",
+                "ubicacion": "Estomba 2345, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.6987669",
+                "longitud": "-62.29157600000002",
+                "telefono": "4881990",
+                "responsable": "Monaco Francesca",
+                "comentarios": [],
+                "tipo": "Comedores"
+            },
+            {
+                "nombre": "CENTRO SOCIAL NATAN",
+                "ubicacion": "Fabian González 2190, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.7148784",
+                "longitud": "-62.320715800000016",
+                "telefono": "154273965",
+                "responsable": "Sensini Dora",
+                "comentarios": [],
+                "tipo": "Comedores"
+            },
+            {
+                "nombre": "CENTRO SOCIAL SEMILLA DE ESPERANZA",
+                "ubicacion": "Rincón 3910, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.7424377",
+                "longitud": "-62.212746100000004",
+                "telefono": "4814109",
+                "responsable": "Juan Elortegui",
+                "comentarios": [],
+                "tipo": "Comedores"
+            },
+            {
+                "nombre": "ALEGRIA DE VIVIR",
+                "ubicacion": "E.Gonzalez 50, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.6869847",
+                "longitud": "-62.27511470000002",
+                "telefono": "4886867",
+                "responsable": "Sr Pariente",
+                "comentarios": [],
+                "tipo": "Ancianos"
+            },
+            {
+                "nombre": "ASOCIACION ALBORADA  (TALLER PROTEGIDO)",
+                "ubicacion": "9 de Julio 924, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.7173584",
+                "longitud": "-62.28533909999999",
+                "telefono": "4562464",
+                "comentarios": [],
+                "tipo": "Personas con discapacidad"
+            },
+            {
+                "nombre": "TALLER AFECTOS ESPECIALES",
+                "ubicacion": "Salinas Chicas 4201, Bahía Blanca, Buenos Aires, Argentina",
+                "latitud": "-38.71400759999999",
+                "longitud": "-62.18879980000003",
+                "telefono": "4861305",
+                "responsable": "-Sra DUO",
+                "comentarios": [],
+                "tipo": "Personas con discapacidad"
             },
         ]
     },
@@ -58,18 +124,18 @@ module.exports = [
         'model': 'User',
         'documents': [
             {
-                "hash" : "a512dd4388e141696e72aaf0d513962cdfe94907c212fcee0ff73923d7deb8a23c78a2855b3de1837abb6c109f95e6635d1f789993ffc61e06f095ae8d04682e",
-                "salt" : "b634167f5cb6183137b470610e06cccd",
-                "email" : "admin@admin.com",
-                "name" : "Admin",
-                "admin" : true,
+                "hash": "a512dd4388e141696e72aaf0d513962cdfe94907c212fcee0ff73923d7deb8a23c78a2855b3de1837abb6c109f95e6635d1f789993ffc61e06f095ae8d04682e",
+                "salt": "b634167f5cb6183137b470610e06cccd",
+                "email": "admin@admin.com",
+                "name": "Admin",
+                "admin": true,
             },
             {
-                "hash" : "daecabb1979ccf8c486c7a06e46f42adce1817e0ce4ac26cca9ad086a178a7e304abdab94c3c6333a39973d491e7bc8d6a495c7c9673caabae6c748cebfe746e",
-                "salt" : "c226c255fb56fa583e6ab079e84bff8c",
-                "email" : "user@user.com",
-                "name" : "User",
-                "admin" : false,
+                "hash": "daecabb1979ccf8c486c7a06e46f42adce1817e0ce4ac26cca9ad086a178a7e304abdab94c3c6333a39973d491e7bc8d6a495c7c9673caabae6c748cebfe746e",
+                "salt": "c226c255fb56fa583e6ab079e84bff8c",
+                "email": "user@user.com",
+                "name": "User",
+                "admin": false,
             }
         ]
     }
